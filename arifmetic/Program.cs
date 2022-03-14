9625cf7b658761039d86907228489f8e8a1ef7e3
Overlord7667 <aspiaro@gmail.com>
2022-03-14 22:28:12 +0300

    Add project files.

---
+++ arifmetic/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace arifmetic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstValue, secondValue;
+            
+
+            Console.WriteLine("Введите первое число");
+            firstValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число");
+            secondValue = double.Parse(Console.ReadLine());
+
+            double result = (firstValue + secondValue);
+            Console.WriteLine( " sum of two numbers =  " + result );
+
+            double result2 = (firstValue + secondValue) / 2;
+            Console.WriteLine(" the arifmetic mean of this number =  " + result2);
+        }
+    }
+}
---
+++ converter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double USDToRUB = 122.12;
+            double USDToCNY = 6.36;
+            double USD;
+
+            Console.WriteLine("Введите сумму в USD ");
+
+            USD = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(USD + " USD в RUB = " + USD * USDToRUB);
+            Console.WriteLine(USD + " USD в CNY = " + USD * USDToCNY);
+        }
+    }
+}
